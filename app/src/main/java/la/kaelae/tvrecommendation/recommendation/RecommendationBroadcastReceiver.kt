6e2fd0e6bd591cdc3e521d8fa95f20cab0529d6a
Yuichi Maekawa <kaelaela.31@gmail.com>
2018-02-04 17:19:19 +0900

    Add BroadcastReceiver

---
+++ app/src/main/java/la/kaelae/tvrecommendation/recommendation/RecommendationBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package la.kaelae.tvrecommendation.recommendation
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.support.media.tv.TvContractCompat
+
+class RecommendationBroadcastReceiver: BroadcastReceiver() {
+  override fun onReceive(context: Context, intent: Intent) {
+    if (intent.action != TvContractCompat.ACTION_INITIALIZE_PROGRAMS) return
+    //TODO start job
+  }
+}
